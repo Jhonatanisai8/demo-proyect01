fbce83abe6882ef358a42586516a971e4c97c916
jhonatan <ojedasnachezjhonatan@gmail.com>
2025-09-17 01:16:57 -0500

    seleccitor por id

---
+++ app.js
@@ -0,0 +1,9 @@
+// seleccionamos un elemento con js 
+const contenedor = document.getElementById('container')
+console.log(contenedor);
+// estructura html que hay dentro del elemento 
+console.log(contenedor.innerHTML);
+const titulo = document.getElementById('titulo')
+console.log(titulo);
+// estructura html que hay dentro del elemento 
+console.log(titulo.innerHTML);
