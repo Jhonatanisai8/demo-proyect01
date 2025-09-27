be095d9fd9e42b953b4d59f8653372b1e22c319d
jhonatan <ojedasnachezjhonatan@gmail.com>
2025-09-26 22:05:27 -0500

    obtener estilos en javascript

--- asignar-estilos.js
+++ asignar-estilos.js
@@ -6,5 +6,7 @@ primerTopping.style.backgroundColor = "blue";
 primerTopping.style.fontSize = "2rem";
 primerTopping.style.color = "#f43";
 primerTopping.style.textTransform = "uppercase";
+let colorFondo = primerTopping.style.backgroundColor;
+console.log(colorFondo);
 
 console.log(primerTopping.style);
