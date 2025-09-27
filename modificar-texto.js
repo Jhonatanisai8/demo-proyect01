fb37dacddceb2ae25100d33a81a0c28a0e39f1f4
jhonatan <ojedasnachezjhonatan@gmail.com>
2025-09-26 22:49:13 -0500

    Modificar el Texto

---
+++ modificar-texto.js
@@ -0,0 +1,9 @@
+console.log("=Modificacion de Texto=");
+let titulo = document.getElementById("titulo");
+let textoTitulo = titulo.innerText;
+
+console.log("Titulo de la pagina web");
+console.log(textoTitulo);
+
+titulo.innerText = 'Mis toppings favoritos';
+console.log(titulo);
