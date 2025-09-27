eda48ef542a9a798686def2ae65fb3ca7555880b
jhonatan <ojedasnachezjhonatan@gmail.com>
2025-09-26 22:04:24 -0500

    Asignar estilo con JavaScript

---
+++ asignar-estilos.js
@@ -0,0 +1,10 @@
+const primerTopping = document.querySelector(".toppings");
+console.log(primerTopping);
+
+// modificamos propiedades css  de primerTopping
+primerTopping.style.backgroundColor = "blue";
+primerTopping.style.fontSize = "2rem";
+primerTopping.style.color = "#f43";
+primerTopping.style.textTransform = "uppercase";
+
+console.log(primerTopping.style);
