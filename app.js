bf882b2911ded0b86dae6c12aeff8844e8f3e455
jhonatan <ojedasnachezjhonatan@gmail.com>
2025-09-26 22:03:48 -0500

    getElementById()

--- app.js
+++ app.js
@@ -1,9 +1,24 @@
-// seleccionamos un elemento con js 
-const contenedor = document.getElementById('container')
-console.log(contenedor);
-// estructura html que hay dentro del elemento 
-console.log(contenedor.innerHTML);
-const titulo = document.getElementById('titulo')
+// seleccionamos un elemento con js
+
+// const contenedor = document.getElementById('container')
+// console.log(contenedor);
+
+// estructura html que hay dentro del elemento
+// console.log(contenedor.innerHTML);
+
+const titulo = document.getElementById("titulo");
+// console.log(titulo);
+
+// estructura html que hay dentro del elemento
+// console.log(titulo.innerHTML);
+
 console.log(titulo);
-// estructura html que hay dentro del elemento 
-console.log(titulo.innerHTML);
+
+// TIPO DE DATOS DE "titulo"
+console.log(`Tipo de dato: ${typeof titulo}`);
+
+//retorna el texto que hay dentro del contenido
+console.log(`Contenido del elemento: ${titulo.innerText}`);
+
+//nombre de la etiqueta
+console.log(`Nombre de la etiqueta: ${titulo.tagName}`);
