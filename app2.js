e5dad2946e25955c9cffbaa2b7c4d05b320b52b7
jhonatan <ojedasnachezjhonatan@gmail.com>
2025-09-26 22:04:05 -0500

    getElementsByTagName()

---
+++ app2.js
@@ -0,0 +1,6 @@
+console.log("METODO TAG-NAME");
+const misToppings = document.getElementsByTagName("li");
+console.log(misToppings);
+
+const toppingsFondoMarron = document.getElementsByClassName('fondo-marron')
+console.log(toppingsFondoMarron);
