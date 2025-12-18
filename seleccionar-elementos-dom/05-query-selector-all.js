b1912fbde92f9c32fc89552083cde5733928bc63
jhonatan <ojedasnachezjhonatan@gmail.com>
2025-12-17 22:18:07 -0500

    Agregar script para seleccionar múltiples elementos con querySelectorAll y actualizar referencias en index.html

---
+++ seleccionar-elementos-dom/05-query-selector-all.js
@@ -0,0 +1,9 @@
+console.log("05-query-selector-all.js");
+const toppingMarron = document.querySelectorAll(".fondo-marron");
+console.log(toppingMarron);
+
+console.log("=================================");
+console.log(toppingMarron[0].innerHTML);
+console.log(toppingMarron[1].innerHTML);
+
+
