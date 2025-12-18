f695c1c57d7ffd4128a3770d8adaccded9e3642a
jhonatan <ojedasnachezjhonatan@gmail.com>
2025-12-17 21:50:42 -0500

    Corregir clases de elementos en la lista de toppings y agregar script para obtener elementos por clase

---
+++ seleccionar-elementos-dom/02-get-elements-by-class.js
@@ -0,0 +1,7 @@
+console.log("02-get-elements-by-class.js");
+const topping = document.getElementsByClassName("topping");
+console.log(topping);
+console.log(typeof topping)
+console.log("==================================");
+console.log("Elemento 0",topping[0]);
+console.log(`Id del primer elemento de la lista: ${topping[0].id}`);
