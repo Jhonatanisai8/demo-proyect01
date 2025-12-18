0d65a70c5ed0507f1f34d79dc9ac8dd04a4b7e76
jhonatan <ojedasnachezjhonatan@gmail.com>
2025-12-17 21:57:13 -0500

    Agregar script para obtener elementos por etiqueta y corregir la inclusión de script por clase

---
+++ seleccionar-elementos-dom/03-get-elements-by-tag.js
@@ -0,0 +1,8 @@
+console.log("03-get-elements-by-tag.js");
+const misToppings = document.getElementsByTagName("li");
+console.log(misToppings);
+
+console.log("===============================================");
+
+const toppingsConFondoMarron = document.getElementsByClassName("fondo-marron");
+console.log(toppingsConFondoMarron);
