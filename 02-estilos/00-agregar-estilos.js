0718a8ee35307b7cdc75506c55708b6624603104
jhonatan <ojedasnachezjhonatan@gmail.com>
2025-12-20 22:04:21 -0500

    Agregar script para aplicar estilos a los elementos de la lista de toppings

--- 02-estilos/00-agregar-estilos.js
+++ 02-estilos/00-agregar-estilos.js
@@ -1,1 +1,13 @@
 console.log("=AGREGANGO ESTILOS UTILIZANDO JS=");
+const primerTopping = document.querySelector(".topping");
+console.log(primerTopping);
+console.log("============================");
+const estilos = primerTopping.style;
+console.log(estilos);
+
+// Agregando estilos
+primerTopping.style.backgroundColor = "blue";
+primerTopping.style.color = "red";
+primerTopping.style.textTransform = "uppercase";
+primerTopping.style.padding = "10px";
+primerTopping.style.borderRadius = "8px";
