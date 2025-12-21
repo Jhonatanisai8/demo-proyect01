930ce3c778c8d8548b6cb103478473ee529f7680
jhonatan <ojedasnachezjhonatan@gmail.com>
2025-12-20 22:17:50 -0500

    Agregar scripts para seleccionar y modificar texto en el DOM

---
+++ 03-texto/00-acceder-texto.js
@@ -0,0 +1,8 @@
+console.log("SELECCIONAR TEXTO EN EL DOM CON JS");
+const listaToppings = document.getElementById("lista-toppings");
+console.log(listaToppings.innerText);
+console.log("=======================");
+console.log(listaToppings.textContent);
+console.log("=======================");
+console.log(listaToppings.innerHTML);
+console.log("=======================");
---
+++ 03-texto/01-modificar-text.js
@@ -0,0 +1,5 @@
+console.log("=MODIFICAR EL TEXTO=");
+const titulo = document.getElementById("titulo");
+console.log("Titulo Original: ", titulo.innerText);
+
+titulo.innerText = "Mis toppings favoritos";
