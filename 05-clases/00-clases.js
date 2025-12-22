196005d98a478f3f9f054be73aa0eda0e84a1d20
jhonatan <ojedasnachezjhonatan@gmail.com>
2025-12-21 22:41:48 -0500

    Agregar script para trabajar con clases en el DOM y actualizar index.html

---
+++ 05-clases/00-clases.js
@@ -0,0 +1,8 @@
+console.log("=CLASES EN EL DOM=");
+const primerTopping = document.querySelector(".topping");
+console.log(primerTopping);
+console.log(primerTopping.innerText);
+console.log("===========clasesPrimerTopping=============");
+const clasesPrimerTopping = primerTopping.classList;
+console.log(clasesPrimerTopping); 
+console.log("========================");
